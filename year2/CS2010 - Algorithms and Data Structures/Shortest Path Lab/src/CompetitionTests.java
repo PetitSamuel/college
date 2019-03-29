9832f9b98a98856cb636a64ca9185ac1bcaf7d39
Samuel Petit <petits@tcd.ie>
2019-03-29 14:11:18 +0000

    finishing comments for lab

--- year2/CS2010 - Algorithms and Data Structures/Shortest Path Lab/src/CompetitionTests.java
+++ year2/CS2010 - Algorithms and Data Structures/Shortest Path Lab/src/CompetitionTests.java
@@ -1,3 +1,53 @@
+/*
+JUSTIFICATIONS :
+CompetitionDijkstra:
+I start by building a new graph object, which came in useful rather than putting everything into one file. This object only uses one
+data structure : an Arraylist to keep track of neighbors
+
+After building the graph, I create an array of Dijkstra objects, which I give the graph in the constructor and it will then compute
+the shortest paths according to dijkstra's algorithm. This Dijkstra object uses a priority queue of edges as it needs to consider
+the smallest distances first, the reason for creating and edge object was so that it would be easy to find from an instance of this object
+the source and destination as well as the distance (weight) for the edge. It is important to note that this object has a custom
+compareTo method which is based on the distance of an edge so that I can be used properly inside the priority queue.
+
+After a list of shortest paths is calculated for a specific source node, I add all of those distances inside another priority queue of edges
+we will use this queue later for finding the ideal positions and destination nodes.
+The idea is that the distances from a specific node to another one should be considered from lowest distance to biggest, and the
+first "destination intersection" that has 3 different paths going there is effectively the best set of starting points and finishing points.
+
+It is pretty obvious here that a priority queue of edges is useful so that we can start with the smallest distances first and then progressively
+make our way to bigger distances.
+The way I keep track of the starting points and finishing points for all of these is with a Hashmap, the key is an integer which represents the
+destination intersection and the body is a list of integers representing the sources intersections.
+
+The first destination that has a list of 3 sources is then effectively the best set of starting points and ending points for the given graph.
+We then assign the longest distance to the fastest walkers and compute the minimum time required.
+
+FloydWarshall:
+It uses exactly the same procedure as described above expect that the list of distances and edges are stored in a matrix rather than an adjacency list
+like in dijkstra.
+
+The difference in computing time might come from the fact that dijkstra's algorithms computes the shortest paths based on
+a specific source, meaning we have to compute it for every single node in this case, unlike floydwarshall's algorithm that computes
+the shortest paths between all nodes (it doesn't consider a source node).
+
+Taking this into account it makes sense that the bigger the graph considered, the more floydwarshall's algorithm is at an advantage
+as it doesn't need as much computing power as computing dijkstra's shortest path for which we would effectively need to compute it for N
+different nodes.
+
+In the case where we need to find the shortest paths between multiple nodes, floyd warshall's algorithm would be a better option than djikstra's algorithm.
+On the other hand, if we need to find a path starting from a specific source then Dijkstra's algorithm would be the algorithm I would choose.
+
+average time for running the tiny EWD file with dijkstra 7753076 (about 7.75 milliseconds)
+average time for running the tiny EWD file with floyd 2282090 (about 2.28 milliseconds)
+dijkstra took 5470986ns more than floyd (about 5millisecond)
+
+average time for running the 1000EWD file with floyd 703443611 (703.4 milliseconds)
+average time for running the 1000EWD file with dijkstra 2171766229 (2171.8 milliseconds)
+dijkstra took 1468322618 ns more than floydwarshall; about 1.5 more seconds (or 1468.3 milliseconds)
+ */
+
+
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
@@ -118,4 +168,45 @@ public class CompetitionTests {
         assertEquals(-1, floyd.timeRequiredforCompetition());
 
     }
+
+    /*
+    @Test
+    public void testTiming () {
+        long start = System.nanoTime();
+        for(int i = 0; i < 10; i++) {
+            CompetitionDijkstra dijkstra = new CompetitionDijkstra("tinyEWD.txt", 50, 50, 50);
+            dijkstra.timeRequiredforCompetition();
+        }
+        long finish = System.nanoTime();
+        long timeElapsed = (finish - start)/10;
+        System.out.println("average time for running the tiny EWD file with dijkstra " + timeElapsed);
+
+        start = System.nanoTime();
+        for(int i = 0; i < 10; i++) {
+            CompetitionFloydWarshall floyd = new CompetitionFloydWarshall("tinyEWD.txt", 100, 100, 100);
+            floyd.timeRequiredforCompetition();
+        }
+        finish = System.nanoTime();
+        timeElapsed = (finish - start)/10;
+        System.out.println("average time for running the tiny EWD file with floyd " + timeElapsed);
+
+        start = System.nanoTime();
+        for(int i = 0; i < 10; i++) {
+            CompetitionDijkstra dijkstra = new CompetitionDijkstra("1000EWD.txt", 50, 50, 50);
+            dijkstra.timeRequiredforCompetition();
+        }
+        finish = System.nanoTime();
+        timeElapsed = (finish - start)/10;
+        System.out.println("average time for running the 1000EWD file with floyd " + timeElapsed);
+
+        start = System.nanoTime();
+        for(int i = 0; i < 10; i++) {
+            CompetitionFloydWarshall floyd = new CompetitionFloydWarshall("1000EWD.txt", 100, 100, 100);
+            floyd.timeRequiredforCompetition();
+        }
+        finish = System.nanoTime();
+        timeElapsed = (finish - start)/10;
+        System.out.println("average time for running the 1000EWD file with dijkstra " + timeElapsed);
+    }
+    */
 }
--- year2/CS2010 - Algorithms and Data Structures/Shortest Path Lab/src/FloydWarshall.java
+++ year2/CS2010 - Algorithms and Data Structures/Shortest Path Lab/src/FloydWarshall.java
@@ -1,44 +1,31 @@
 public class FloydWarshall {
-    public double[][] distTo;         // distTo[v][w] = length of shortest v->w path
-    private Edge[][] edgeTo;   // edgeTo[v][w] = last edge on shortest v->w path
+    public double[][] distTo;
+    private Edge[][] edgeTo;
 
-    /**
-     * Computes a shortest paths tree from each vertex to to every other vertex in
-     * the edge-weighted digraph {@code G}. If no such shortest path exists for
-     * some pair of vertices, it computes a negative cycle.
-     *
-     * @param G the edge-weighted digraph
-     */
     public FloydWarshall(Graph G) {
         int V = G.countVertices();
         distTo = new double[V][V];
         edgeTo = new Edge[V][V];
 
-        // initialize distances to infinity
         for (int v = 0; v < V; v++) {
             for (int w = 0; w < V; w++) {
                 distTo[v][w] = Double.MAX_VALUE;
             }
         }
-
-        // initialize distances using edge-weighted digraph's
         for (int v = 0; v < G.countVertices(); v++) {
             for (Edge e : G.adj[v]) {
                 distTo[e.from()][e.to()] = e.distance();
                 edgeTo[e.from()][e.to()] = e;
             }
-            // in case of self-loops
+
             if (distTo[v][v] >= 0.0) {
                 distTo[v][v] = 0.0;
                 edgeTo[v][v] = null;
             }
-        }
 
-        // Floyd-Warshall updates
+        }
         for (int i = 0; i < V; i++) {
-            // compute shortest paths using only 0, 1, ..., i as intermediate vertices
             for (int v = 0; v < V; v++) {
-                if (edgeTo[v][i] == null) continue;  // optimization
                 for (int w = 0; w < V; w++) {
                     if (distTo[v][w] > distTo[v][i] + distTo[i][w]) {
                         distTo[v][w] = distTo[v][i] + distTo[i][w];
