289dfc3b923849718a8e5bb9cf5ec9d20254dec6
Samuel Petit <petits@tcd.ie>
2019-03-29 15:26:49 +0000

    adding concurrent systems lab 4 and polishing algorithms lab zip file

---
+++ year2/CS2016 - Concurrent Systems and Operating Systems/practical4/proc.c
@@ -0,0 +1,110 @@
+// ONLY MODIFY CODE BELOW THE NEXT OCCURENCE OF THE FOLLOWING LINE !
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include "types.h"
+#include "defs.h"
+#include <stdio.h>
+#include "proc.h"
+
+#define NCPU 1
+
+struct cpu cpus[NCPU];
+int ncpu;
+
+void printstate(enum procstate pstate){ // DO NOT MODIFY
+  switch(pstate) {
+    case UNUSED   : printf("UNUSED");   break;
+    case EMBRYO   : printf("EMBRYO");   break;
+    case SLEEPING : printf("SLEEPING"); break;
+    case RUNNABLE : printf("RUNNABLE"); break;
+    case RUNNING  : printf("RUNNING");  break;
+    case ZOMBIE   : printf("ZOMBIE");   break;
+    default       : printf("????????");
+  }
+}
+
+// Per-CPU process scheduler.
+// Each CPU calls scheduler() after setting itself up.
+// Scheduler never returns.  It loops, doing:
+//  - choose a process to run
+//  - swtch to start running that process
+//  - eventually that process transfers control
+//      via swtch back to the scheduler.
+
+// local to scheduler in xv6, but here we need them to persist outside,
+// because while xv6 runs scheduler as a "coroutine" via swtch,
+// here swtch is just a regular procedure call.
+struct proc *p;
+struct cpu *c = cpus;
+
+// +++++++ ONLY MODIFY BELOW THIS LINE ++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// declaring counts array
+int counts[NPROC];
+int start;
+
+void
+scheduler(void)
+{ int runnableFound; // DO NOT MODIFY/DELETE
+
+  c->proc = 0;
+
+  runnableFound = 1 ; // force one pass over ptable
+
+  // make sure to initialise array
+  if (!start) {
+    start = 1;
+    for(int i = 0; i < NPROC; i++) {
+      counts[i] = 0;
+    }
+  }
+
+  while(runnableFound){ // DO NOT MODIFY
+    // Enable interrupts on this processor.
+    // sti();
+    runnableFound = 0; // DO NOT MODIFY
+    // Loop over process table looking for process to run.
+    // acquire(&ptable.lock);
+    // maximum integer possible
+    int min = 2147483647;
+    int index = 0;
+
+    // go through runnable processes
+    for(int i = 0;  i < NPROC; i++) {
+       if(ptable.proc[i].state != RUNNABLE)
+            continue;
+
+        // find the index that has been run the less times
+        if(counts[i]  < min) {
+            runnableFound = 1;
+            min = counts[i];
+            index = i;
+        }
+    }
+    // if we found something to run : 
+    if(runnableFound) {
+      // increment & get the process
+      counts[index]++;
+      p = &ptable.proc[index];
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      //switchuvm(p);
+      p->state = RUNNING;
+
+
+      swtch(p);
+      // p->state should not be running on return here.
+      //switchkvm();
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    // release(&ptable.lock);
+
+  }
+  printf("No RUNNABLE process!\n");
+}
