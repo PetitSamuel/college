e9c98c035e6d24521beacb87b85469faf0d9a688
PetitSamuel <petits@tcd.ie>
2020-02-21 00:28:56 +0000

    adding year 3 files from hkust and updating stats assignments

--- hkust/popl/lab1/test.sml
+++
@@ -1,3 +0,0 @@
-fun checkTriangle (x,n) = if x = n*(n+1) div 2 then true else if x > n*(n+1) div 2 then checkTriangle(x, n+1) else false;
-
-fun isTriangle(x) = checkTriangle(x,0);
--- hkust/popl/prolog/solution10.pl
+++
@@ -1,7 +0,0 @@
-%list_reverse
-list_reverse([],L) :- append([],[],L).
-list_reverse([H|T],L) :- list_reverse(T,L1),append(L1,[H],L).
-
-%prefix
-list_prefix([],_). %base case
-list_prefix([X|Xs],[X|Ys]) :- list_prefix(Xs,Ys). %inductive case
